137a7224fab5d0a75de04430ccc5d7293b82e09c
Ümit Yasin Çoban <umityasincoban@gmail.com>
2022-08-24 00:24:34 +0300

    added play game button

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
-var randomFirstNumber = Math.random();
+document.querySelector(".btn").addEventListener("click",playGame);
 
-randomFirstNumber = Math.floor(randomFirstNumber * 6) +1;
 
-console.log(randomFirstNumber);
 
-document.querySelector(".img1").setAttribute("src","images/dice"+randomFirstNumber+".png");
+function playGame() {
+    var randomFirstNumber = Math.random();
 
-var randomSecondNumber = Math.random();
-
-randomSecondNumber = Math.floor(randomSecondNumber * 6) +1;
-
-document.querySelector(".img2").setAttribute("src","images/dice"+randomSecondNumber+".png");
-
-if(randomFirstNumber > randomSecondNumber){
-    document.querySelector("h1").innerHTML = "Player 1 Wins";
-}else if(randomSecondNumber > randomFirstNumber){
-    document.querySelector("h1").innerHTML = "Player 2 Wins";
-}else{
-    document.querySelector("h1").innerHTML = "Draw!";
+    randomFirstNumber = Math.floor(randomFirstNumber * 6) +1;
+    
+    console.log(randomFirstNumber);
+    
+    document.querySelector(".img1").setAttribute("src","images/dice"+randomFirstNumber+".png");
+    
+    var randomSecondNumber = Math.random();
+    
+    randomSecondNumber = Math.floor(randomSecondNumber * 6) +1;
+    
+    document.querySelector(".img2").setAttribute("src","images/dice"+randomSecondNumber+".png");
+    
+    if(randomFirstNumber > randomSecondNumber){
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    }else if(randomSecondNumber > randomFirstNumber){
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    }else{
+        document.querySelector("h1").innerHTML = "Draw!";
+    } 
 }
+
